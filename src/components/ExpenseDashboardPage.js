6f214724f099075c9302a2ab16ad394b7a8efbf6
sohn12 <sohan.reddy12@gmail.com>
2018-12-25 13:00:28 +0530

    The First commit

---
+++ src/components/ExpenseDashboardPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ExpenseList from "./ExpenseList";
+import ExpenseListFilters from "./ExpensesListFilters";
+
+const ExpenseDashboardPage = () => (
+  <div>
+    <ExpenseListFilters />
+    <ExpenseList />
+  </div>
+);
+export default ExpenseDashboardPage;
